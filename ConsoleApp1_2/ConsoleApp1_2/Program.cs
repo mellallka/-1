ad97face127adf9ed9dad27ffee238a8cd80d866
mellallka <emeleskova5@gmail.com>
2024-10-06 21:29:14 +0300

    Add files via upload

---
+++ ConsoleApp1_2/ConsoleApp1_2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество секунд: ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int hours = x / 3600;
+        int minute = (x - hours * 3600) / 60;
+        int second = x % 60;
+        Console.WriteLine("Из них часы: " + hours + ", минуты: " + minute + ", секунды: " + second);
+    }
+}
