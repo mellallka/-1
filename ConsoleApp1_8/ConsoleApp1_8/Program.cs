ab08e9c3f829387fd632e7026333292ac9d45d91
mellallka <emeleskova5@gmail.com>
2024-10-06 21:35:58 +0300

    Add files via upload

---
+++ ConsoleApp1_8/ConsoleApp1_8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите действительное число x: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        double result = x * (x * (x * (3 * x - 5) + 2) - 1) + 7;
+        Console.WriteLine("Результат: " + result);
+    }
+}
