be5d4fd02c047eb0750c98499b4ebb228320818b
mellallka <emeleskova5@gmail.com>
2024-10-06 21:32:26 +0300

    Add files via upload

---
+++ ConsoleApp1_5/ConsoleApp1_5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Длина первого катета:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Длина второго катет:");
+        int b = int.Parse(Console.ReadLine());
+        double area = 0.5 * a * b;
+        double c = Math.Sqrt(a * a + b * b);
+        double perimeter = a + b + c;
+        Console.WriteLine("Площадь: " + area);
+        Console.WriteLine("Периметр: " + perimeter);
+    }
+}
