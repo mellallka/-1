00eebeca43b7d5e79bccb34d40d7ab8a712a9ab1
mellallka <emeleskova5@gmail.com>
2024-10-06 21:30:00 +0300

    Add files via upload

---
+++ ConsoleApp1_3/ConsoleApp1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("часы:");
+        int h = int.Parse(Console.ReadLine());
+        Console.WriteLine("минуты:");
+        int m = int.Parse(Console.ReadLine());
+        Console.WriteLine("Cекунды:");
+        int s = int.Parse(Console.ReadLine());
+
+        double angle = (h * 30) + (m * 0.5) + (s * 0.5 / 60);
+        Console.WriteLine("Угол между началом суток и положением часовой стрелки: " + angle + " градусов.");
+    }
+}
