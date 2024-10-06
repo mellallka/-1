f753066eb23b9e66349c44d621c257ae69e6fa83
mellallka <emeleskova5@gmail.com>
2024-10-06 21:31:33 +0300

    Add files via upload

---
+++ ConsoleApp1_4/ConsoleApp1_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 6;
+        int b = 10;
+
+        Console.WriteLine("До обмена a: " + a + " b: " + b);
+        a = a + b;
+        b = a - b; 
+        a = a - b; 
+        Console.WriteLine("После обмена a: " + a + " b: " + b);
+    }
+}
