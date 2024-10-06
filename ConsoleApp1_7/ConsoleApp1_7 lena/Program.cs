2d2bf51a1cacd7035f6d2e8c89ffb2ed7f6cdb79
mellallka <emeleskova5@gmail.com>
2024-10-06 21:35:04 +0300

    Add files via upload

---
+++ ConsoleApp1_7/ConsoleApp1_7 lena/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите трёхзначное число: ");
+        string number = Console.ReadLine();
+        string reversedNumber = number[2].ToString() + number[1].ToString() + number[0].ToString();
+        Console.WriteLine("Число в обратном порядке: " + reversedNumber);
+    }
+}
