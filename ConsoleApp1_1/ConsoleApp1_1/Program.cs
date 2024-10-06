55b4b53b23bce9416b3958a666ec3bdc86ac5d0b
mellallka <emeleskova5@gmail.com>
2024-10-06 21:24:16 +0300

    Add files via upload

---
+++ ConsoleApp1_1/ConsoleApp1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        double x = random.NextDouble() * 100;
+        double fractionalPart = x - Math.Floor(x); // получив целую часть числа отнимаем от вещественного
+        int d = (int)(fractionalPart * 10); // умножаем чтобы оставить только первое число
+        Console.WriteLine("Случайное число: " + x);
+        Console.WriteLine("Первая цифра дробной части числа: " + d);
+    }
+}
