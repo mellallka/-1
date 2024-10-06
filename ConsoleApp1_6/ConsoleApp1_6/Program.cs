41804604d5b7b6a1d5ac86834b883b1b324901f3
mellallka <emeleskova5@gmail.com>
2024-10-06 21:33:53 +0300

    Add files via upload

---
+++ ConsoleApp1_6/ConsoleApp1_6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = int.Parse(Console.ReadLine());
+        int fr = x % 10;
+        int thfr = x % 100;
+        int th = thfr / 10;
+        int fisc = x / 100;
+        int sc = fisc % 10;
+        int fi = x / 1000;
+        int end = fi * sc * th * fr;
+        Console.WriteLine(end);
+        
+    }
+}
